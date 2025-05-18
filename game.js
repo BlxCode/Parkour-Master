2eb4418eccb4064a84d48bf635cc3de58ba3b9b3
Blxm <bloxdmasterbloxdacc@gmail.com>
2025-05-18 15:06:27 -0700

    update script

--- game.js
+++ game.js
@@ -5,3 +5,11 @@ new Phaser.Game({
     }
   }
 });
+const config = {
+    type: Phaser.AUTO, // automatically detect browser WebGL support
+    width: window.innerWidth, // canvas width
+    height: window.innerHeight, // canvas height
+    backgroundColor: '#dbfff3', // canvas background color
+    parent: 'phaser-example', // parent DOM element
+    scene: Example // 
+};
